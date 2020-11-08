ede11a54c30fd76e04514e5db81445c8c1e42a58
euphmat <euphmat@gmail.com>
2020-11-09 08:54:14 +0900

    Update Build.sh, その他

--- CLI_Tool/src/SUMMARY.md
+++ CLI_Tool/src/SUMMARY.md
@@ -1,3 +1,4 @@
 # Summary
 
-- [Chapter 1](./chapter_1.md)
+- [Index](./index.md)
+- [mdbook](./mdbook.md)
--- build.sh
+++ build.sh
@@ -2,15 +2,21 @@
 set -Ceuo pipefail
 
 function main(){
+        #script_dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd -P)"
+        #cd $script_dir
         dirs=`ls -l | awk '$1 ~ /d/ {print $9 }'`
-
         for dir in $dirs;
         do
                 cd $dir
-                echo -e "\033[0;32m[$dir]\033[0;39m"
-                mdbook build
+                echo -en "\033[0;34m[$dir]:\033[0;39m"
+                if [[ `mdbook build > /dev/null 2>&1 ; echo $?` = 0 ]]; then
+                        echo -e " \033[0;32m✔\033[0;39m"
+                else
+                        echo -e " \033[0;31m×\033[0;39m"
+                fi
                 cd ..
         done
+
 }
 
 main
